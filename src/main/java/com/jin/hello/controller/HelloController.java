1b9c6a8c50e76c228dd6fc78449d84c5b0f728b7
solpi2 <parkchini@gmail.com>
2025-06-25 18:14:57 +0900

    first commit2

--- src/main/java/com/jin/hello/controller/HelloController.java
+++ src/main/java/com/jin/hello/controller/HelloController.java
@@ -2,14 +2,12 @@ package com.jin.hello.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
     @GetMapping("/")
-    public String hello(Model model) {
-        model.addAttribute("name", "지니");
+    public String hello() {
         return "Hello";
     }
 
