eda1b182b1b72704704194323e023d61bbf081cb
solpi2 <parkchini@gmail.com>
2025-06-25 15:36:22 +0900

    first commit1

--- src/main/java/com/jin/hello/controller/HelloController.java
+++ src/main/java/com/jin/hello/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "hello";
+        return "Hello";
     }
 
 
